a057032b4cc92ae4d742ca79be76a5d3dbf5ea7b
rakibul1 <mrakibul70@gmail.com>
2018-11-04 21:50:58 +0600

    First commit

---
+++ Space War Arcade Game/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Space_War_Arcade_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 81);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(722, 416);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(683, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(631, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Life:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(261, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(234, 157);
+            this.panel2.TabIndex = 5;
+            this.panel2.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(65, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Game Over";
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Space_War_Arcade_Game.Properties.Resources.Exit;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(136, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Space_War_Arcade_Game.Properties.Resources.playAgain;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(17, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Space_War_Arcade_Game.Properties.Resources.ship;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 117);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 6;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(85, 12);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer2.TabIndex = 7;
+            this.axWindowsMediaPlayer2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 498);
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+    }
+}
+
---
+++ Space War Arcade Game/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_War_Arcade_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                moveup();
+            }
+            else if(e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                movedown();
+            }
+            else if(e.KeyCode == Keys.Space)
+            {
+                fire();
+            }
+        }
+
+        private void moveup()
+        {
+            Point pos = pictureBox1.Location;
+            if(pos.Y > 0)
+            {
+                pos.Y -= 20;
+                pictureBox1.ImageLocation = @"ship/left.png";
+            }
+            pictureBox1.Location = pos;
+            
+        }
+
+        private void movedown()
+        {
+            Point pos = pictureBox1.Location;
+            if(pos.Y < panel1.Height-pictureBox1.Height)
+            {
+                pos.Y += 20;
+                pictureBox1.ImageLocation = @"ship/right.png";
+            }
+            pictureBox1.Location = pos;
+            
+        }
+
+        private void fire()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Height = 15;
+            bullet.Width = 25;
+            bullet.ImageLocation = @"ship/fire.png";
+            bullet.Location = new Point(pictureBox1.Location.X + pictureBox1.Width, pictureBox1.Location.Y + pictureBox1.Height / 2);
+            bullet.Name = "bullet";
+            panel1.Controls.Add(bullet);
+        }
+
+        int kindOfEnemies = 4;
+
+        private void CreateEnemies()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(1, kindOfEnemies + 1);
+            PictureBox enamy = new PictureBox();
+            int loc = rnd.Next(0, panel1.Height - enamy.Height);
+            enamy.SizeMode = PictureBoxSizeMode.StretchImage;
+            enamy.ImageLocation = "Aliens/"+x+".png";
+            enamy.Location = new Point(panel1.Width, loc);
+            enamy.Name = "enamy";
+            panel1.Controls.Add(enamy);
+
+        }
+
+        private void createstar()
+        {
+            Random rnd = new Random();
+            PictureBox star = new PictureBox();
+            star.Width = 10;
+            star.Height = 10;
+            int loc = rnd.Next(0, panel1.Height);
+            star.ImageLocation = @"star/star.png";
+            star.SizeMode = PictureBoxSizeMode.StretchImage;
+            star.Location = new Point(panel1.Width - star.Width, loc);
+            star.Name = "star";
+            star.BackColor = Color.Transparent;
+            panel1.Controls.Add(star);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Down || e.KeyCode == Keys.W)
+            {
+                pictureBox1.ImageLocation = @"ship/ship.png";
+            }
+
+           else if (e.KeyCode == Keys.Up || e.KeyCode == Keys.S)
+            {
+                pictureBox1.ImageLocation = @"ship/ship.png";
+            }
+        }
+
+        int counter = 0, counter1 = 0, score = 0,life = 3;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            counter1++;
+            label3.Text = life.ToString();
+            for(int i=0; i<panel1.Controls.Count; i++)
+            {
+                PictureBox pb = ((PictureBox)panel1.Controls[i]); 
+
+                if(panel1.Controls[i].Name == "bullet")
+                {
+                    if(pb.Location.X+pb.Width > panel1.Width)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+                    else
+                    {
+                        pb.Location = new Point(pb.Location.X + 5, pb.Location.Y);
+                    }
+
+                    for(int x = 0; x<panel1.Controls.Count; x++)
+                    {
+                        if(panel1.Controls[x].Name == "enamy")
+                        {
+                            if(pb.Location.X + pb.Width > panel1.Controls[x].Location.X && pb.Location.X + pb.Width < panel1.Controls[x].Location.X + panel1.Controls[x].Width)
+                            {
+                                if(pb.Location.Y+pb.Height>panel1.Controls[x].Location.Y && pb.Location.Y + pb.Height < panel1.Controls[x].Location.Y + panel1.Controls[x].Width)
+                                {
+                                    panel1.Controls.RemoveAt(x);
+                                    panel1.Controls.Remove(pb);
+                                    score++;
+                                    label2.Text = score.ToString();
+                                }
+                            }
+                        }
+                    }
+                }
+
+                else if (panel1.Controls[i].Name == "enamy")
+                {
+                    if (pb.Location.X < 0)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+                    else
+                    {
+                        pb.Location = new Point(pb.Location.X - 5, pb.Location.Y);
+
+                        if(pb.Location.X > pictureBox1.Location.X && pb.Location.X < pictureBox1.Location.X + pictureBox1.Width)
+                        {
+                            if(pb.Location.Y > pictureBox1.Location.Y && pb.Location.Y < pictureBox1.Location.Y + pictureBox1.Height)
+                            {
+                                panel1.Controls.RemoveAt(i);
+                                life--;
+                                axWindowsMediaPlayer1.URL = @"sounds/explode.mp3";
+                                axWindowsMediaPlayer1.Ctlcontrols.play();
+
+                                if(life == 0)
+                                {
+                                    timer1.Stop();
+                                    panel2.Visible = true;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                else if (panel1.Controls[i].Name == "star")
+                {
+                    if (pb.Location.X < 0)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+                    else
+                    {
+                        pb.Location = new Point(pb.Location.X - 5, pb.Location.Y);
+                    }
+                }
+            }
+            if (counter == 50)
+            {
+                counter = 0;     
+                CreateEnemies();
+            }
+            if(counter1 == 20)
+            {
+                counter1 = 0;
+                createstar();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            axWindowsMediaPlayer2.URL = @"sounds/background.mp3";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            playagain();
+        }
+
+        private void playagain()
+        {
+            if(Application.OpenForms[0] == this)
+            {
+                Application.Restart();
+            }
+        }
+
+        private void count()
+        {
+            if (score >= 0 && score <= 10)
+            {
+                if (counter == 50)
+                {
+                    counter = 0;
+                    CreateEnemies();
+                }
+            }
+
+            else if (score > 10 && score <= 20)
+            {
+                if (counter == 30)
+                {
+                    counter = 0;
+                    CreateEnemies();
+                }
+            }
+
+            if (score > 20)
+            {
+                if (counter == 10)
+                {
+                    counter = 0;
+                    CreateEnemies();
+                }
+            }
+        }
+    }
+}
